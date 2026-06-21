296449109194aa9426f162ce019c1ec84d6c8da9
lluthfiamalia <lluthfiamalia14@gmail.com>
2026-06-21 18:55:24 +0700

    feat (patients): fix import userModule in patientModule

--- src/patients/patients.module.ts
+++ src/patients/patients.module.ts
@@ -1,5 +1,6 @@
 import { Module } from '@nestjs/common';
 import { MongoloquentModule } from '@mongoloquent/nestjs';
+import { UsersModule } from '../users/users.module';
 import { PatientProfile } from './models/patient-profile.model';
 import { PatientPmo } from './models/patient-pmo.model';
 import { PatientsService } from './patients.service';
@@ -7,7 +8,8 @@ import { PatientsIndexService } from './patients-index.service';
 import { PatientsController } from './patients.controller';
 
 @Module({
-  imports: [MongoloquentModule.forFeature([PatientProfile, PatientPmo])],
+  imports: [MongoloquentModule.forFeature([PatientProfile, PatientPmo]),
+UsersModule],
   controllers: [PatientsController],
   providers: [PatientsService, PatientsIndexService],
   exports: [PatientsIndexService, MongoloquentModule],
